b91da0385c9c3d89de1f589e11d72079c9eb2a7b
PatyTheImp <69981887+PatyTheImp@users.noreply.github.com>
2021-05-10 08:09:57 +0100

    Add files via upload

---
+++ PrimeiroExemplo/PrimeiroExemplo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrimeiroExemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float w = 30.2f;
+            string texto = "Hello world!";
+
+            Console.WriteLine(w);
+
+            ImprimeNome(texto);
+        }
+
+        public static void ImprimeNome(string nome)
+        {
+            Console.WriteLine("{0}", nome);
+        }
+
+        public static int SomaValores(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
